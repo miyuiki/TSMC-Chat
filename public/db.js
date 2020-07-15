f4fce595d171c3582e0977137b983cada3a3e412
miyuiki <a121406@gmail.com>
2020-07-15 14:51:24 +0800

    add css

--- public/db.js
+++ public/db.js
@@ -1,23 +1,13 @@
 var firebase;
 var $name = ''
 
-$(document).ready(function () {
-    // var form = $('form');
-    var submit = $('button[type="submit"]');
-    submit.on('click', function (e) {
-        e.preventDefault();
-        if($('#inputNmae').val() == ""){
-            // $('#show_error').show();
-            return false;
-        }
-        $name = $('#inputNmae').val();
-    });
-});
 $(function () {
     var $btn = $('#btn'),
         $show = $('#show'),
-        $reset = $('#reset')
-        $clean = $('#clean');
+        $reset = $('#reset'),
+        $clean = $('#clean'),
+        $content = $('#content'),
+        $setName = $('#setName');
 
     var config = {
         apiKey: "AIzaSyCAtEgAwEuuLlr3WYfxsX9ZMW9BT0GuRdI",
@@ -31,30 +21,147 @@ $(function () {
     firebase.initializeApp(config);
     var database = firebase.database().ref();
 
-    $btn.on('click', function () {
+    function write() {
+        var date = new Date();
+        var h = date.getHours();
+        var m = date.getMinutes();
+        var s = date.getSeconds();
+        var ms = date.getTime();
+        if (h < 10) {
+            h = '0' + h;
+        }
+        if (m < 10) {
+            m = '0' + m;
+        }
+        if (s < 10) {
+            s = '0' + s;
+        }
+        var now = h + ':' + m + ':' + s;
         var postData = {
-            name: $name,
-            content: $('#content').val()
+            name: $('#inputNmae').val(),
+            content: $('#content').val(),
+            time: now,
+            id: 'id' + ms
         };
         database.push(postData);
+        $content.val('');
+    }
+
+    $setName.on('click', function (e) {
+        e.preventDefault();
+        if ($('#inputNmae').val() == "") {
+            // $('#show_error').show();
+            return false;
+        }
+        $name = $('#inputNmae').val();
+    });
+
+    $btn.on('click', function () {
+        write();
+    });
+
+    $content.on('keydown', function (e) {
+        if (e.keyCode == 13) {
+            write();
+        }
     });
 
     $clean.on('click', function () {
         database.remove();
+        $show.html('');
+        // location.reload();
     });
 
-    $reset.on('click', function() {
+    $reset.on('click', function () {
         // alert('work')
         $('#exampleModal').modal('show')
     });
 
-    database.on('value', function (snapshot) {
-        $show.html('');
-        var arr = [];
+    database.once('value', function (snapshot) {
+        // $show.html('');
+        // var arr = [];
         for (var i in snapshot.val()) {
-            arr.push(snapshot.val()[i]);
-            $show.append('<div class="row"><span class="rounded">' + snapshot.val()[i].name + ':' + snapshot.val()[i].content + '</span></div>');
+            var name = snapshot.val()[i].name;
+            var content = snapshot.val()[i].content;
+            var time = snapshot.val()[i].time;
+            if (name == $name) {
+                var html = '<div class="row justify-content-end mt-3">\
+                            <div class="toast fade show" role="alert" aria-live="assertive" aria-atomic="true" style="width: 100%;">\
+                                <div class="toast-header">\
+                                    <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" \
+                                    xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">\
+                                        <rect width="100%" height="100%" fill="#007aff"></rect>\
+                                    </svg>\
+                                    <strong class="mr-auto">' + name + '</strong>\
+                                    <small class="text-muted">' + time + '</small>\
+                                </div>\
+                                <div class="toast-body">' + content + '</div>\
+                            </div>\
+                        </div>'
+                // arr.push(snapshot.val()[i]);
+                $show.append(html);
+            }
+            else {
+                var html = '<div class="row mt-3">\
+                            <div class="toast fade show" role="alert" aria-live="assertive" aria-atomic="true" style="width: 100%;">\
+                                <div class="toast-header">\
+                                    <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" \
+                                    xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">\
+                                        <rect width="100%" height="100%" fill="#007aff"></rect>\
+                                    </svg>\
+                                    <strong class="mr-auto">' + name + '</strong>\
+                                    <small class="text-muted">' + time + '</small>\
+                                </div>\
+                                <div class="toast-body">' + content + '</div>\
+                            </div>\
+                        </div>'
+                // arr.push(snapshot.val()[i]);
+                $show.append(html);
+            }
         }
     });
 
+    database.limitToLast(1).on('value', function (snapshot) {
+        // $show.html('');
+        // var arr = [];
+        for (var i in snapshot.val()) {
+            var name = snapshot.val()[i].name;
+            var content = snapshot.val()[i].content;
+            var time = snapshot.val()[i].time;
+            if (name === $name) {
+                var html = '<div class="row justify-content-end mt-3">\
+                            <div class="toast fade show" role="alert" aria-live="assertive" aria-atomic="true" style="width: 100%;">\
+                                <div class="toast-header">\
+                                    <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" \
+                                    xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">\
+                                        <rect width="100%" height="100%" fill="#007aff"></rect>\
+                                    </svg>\
+                                    <strong class="mr-auto">' + name + '</strong>\
+                                    <small class="text-muted">' + time + '</small>\
+                                </div>\
+                                <div class="toast-body">' + content + '</div>\
+                            </div>\
+                        </div>'
+                // arr.push(snapshot.val()[i]);
+                $show.append(html);
+            }
+            else {
+                var html = '<div class="row mt-3">\
+                            <div class="toast fade show" role="alert" aria-live="assertive" aria-atomic="true" style="width: 100%;">\
+                                <div class="toast-header">\
+                                    <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" \
+                                    xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">\
+                                        <rect width="100%" height="100%" fill="#007aff"></rect>\
+                                    </svg>\
+                                    <strong class="mr-auto">' + name + '</strong>\
+                                    <small class="text-muted">' + time + '</small>\
+                                </div>\
+                                <div class="toast-body">' + content + '</div>\
+                            </div>\
+                        </div>'
+                // arr.push(snapshot.val()[i]);
+                $show.append(html);
+            }
+        }
+    });
 });
